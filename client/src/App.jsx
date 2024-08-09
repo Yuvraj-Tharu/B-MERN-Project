1f93fe333bc368e9d7e4c518172160a791009f96
November414241 <yuvrajtharu123@gmail.com>
2024-08-09 14:12:44 +0545

    Creating the navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,24 @@
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Projects from "./pages/Projects";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Navbar />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,5 +1,57 @@
 import React from "react";
+import { Button, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
+import { CiSearch } from "react-icons/ci";
+import { FaMoon } from "react-icons/fa";
 
-export default function Navbar() {
-  return <div></div>;
+export default function Header() {
+  const path = useLocation().pathname;
+
+  return (
+    <>
+      <Navbar className="border-b-2">
+        <Link
+          to="/"
+          className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
+        >
+          <span className="px-2 py-1 bg-gradient-to-r from from-indigo-600 via-purple-500 to-pink-500 rounded-lg text-white">
+            Yuvraj's
+          </span>
+          Blog
+        </Link>
+        <form>
+          <TextInput
+            className="hidden lg:inline-block"
+            type="text"
+            placeholder="search"
+            rightIcon={CiSearch}
+          />
+        </form>
+        <Button className="w-12 h-10 ld:hidden" color="gray" pill>
+          <CiSearch />
+        </Button>
+
+        <div className="flex gap-2 md:order-2">
+          <Button className="h-10 w-12 hidden sm:inline" color="gray" pill>
+            <FaMoon />
+          </Button>
+          <Button gradientDuoTone="purpleToBlue">
+            <Link to="SignIn">SignIn</Link>
+          </Button>
+          <Navbar.Toggle />
+        </div>
+        <Navbar.Collapse>
+          <Navbar.Link active={path == "/"} as={"div"}>
+            <Link to="/home">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path == "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={path == ".projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
 }
